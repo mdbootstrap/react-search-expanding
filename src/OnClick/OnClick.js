6743aba6d01b8e44b3ef2f2ff3accc017601dd12
igor_przybysz <i.przybysz@mdbootstrap.com>
2022-08-22 12:33:50 +0200

    Making corrections

--- src/OnClick/OnClick.js
+++ src/OnClick/OnClick.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { MDBContainer} from "mdb-react-ui-kit";
+import { MDBContainer } from "mdb-react-ui-kit";
 import "./on-click.css";
 
 export default function OnClick() {
   return (
     <MDBContainer className="py-5">
-       <input type="text" className="search-click" placeholder="search here..." />
+      <input
+        type="text"
+        className="search-click"
+        placeholder="search here..."
+      />
     </MDBContainer>
   );
 }
--- src/OnHover/OnHover.js
+++ src/OnHover/OnHover.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { MDBContainer} from "mdb-react-ui-kit";
+import { MDBContainer } from "mdb-react-ui-kit";
 import "./on-hover.css";
 
 export default function OnHover() {
   return (
     <MDBContainer className="py-5">
-       <input type="text" className="search-hover" placeholder="search here..." />
+      <input
+        type="text"
+        className="search-hover"
+        placeholder="search here..."
+      />
     </MDBContainer>
   );
 }
